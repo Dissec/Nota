38a735a1d413b99971dc799a170bb3234f7f70fe
Dissec <104654480+Dissec@users.noreply.github.com>
2024-01-28 21:01:48 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,102 @@
+-- Made by @Astin in www.mycompiler.io
+local PaddingX = 15
+local GradesWeight = {
+    ['D'] = 1,
+    ['D+'] = 2,
+    ['C'] = 3,
+    ['C+'] = 4,
+    ['B'] = 5,
+    ['B+'] = 7.5,
+    ['A'] = 10,
+    ['A'] = 17.5,
+    ['S'] = 25
+}
+
+local StudentsData = {
+    [1] = {
+        'B', '1h 3m', 'faust'
+    },
+
+    [2] = {
+        'B', '1h 22m', 'EmeDeEse'
+    },
+
+    [3] = {
+        'D+', '1h 41m', 'Noox'
+    }
+}
+
+local Keys = {
+    'Rank', 'Tempo', 'Nome'
+}
+
+local Output = ''
+
+function GenerateFloor(Up)
+    local Ground = ''
+    
+    for i=1, PaddingX * #Keys + (#Keys + 1) do
+        Ground = Ground..'-'
+    end
+
+    if Up then
+        Output = Ground..'\n'..Output
+    elseif not Up then
+        Output = Output..'\n'..Ground..'\n'
+    end
+end
+
+function GenerateRooms(Hollow, Data)
+    if Hollow then
+        for Index, Key in pairs(Keys) do
+            Output = Output..'|'
+            
+            for i=1, PaddingX do
+                Output = Output..' '
+            end
+        end
+    elseif not Hollow then
+        for Index, Key in pairs(Data) do
+            Output = Output..'|'
+
+            local Position = math.floor(((PaddingX - string.len(Key)) / 2) + 0.5)
+            local Compensation = (PaddingX - Position - string.len(Key))
+            
+            for i=1, Position do
+                Output = Output..' '
+            end
+            
+            Output = Output..Key
+
+            for i=1, Compensation do
+                Output = Output..' '
+            end
+        end
+    end
+
+    Output = Output..'|'
+end
+
+function Sort()
+    table.sort(StudentsData, function(t1, t2)                
+       return GradesWeight[t1[1]] > GradesWeight[t2[1]]
+    end)
+end
+
+function GenerateLeaderboards()
+    GenerateFloor(true)
+    GenerateRooms(false, Keys)
+    Sort()
+    
+    for Index, Data in ipairs(StudentsData) do
+        Output = Output..'\n'
+        
+        GenerateRooms(false, Data)
+    end
+    
+    GenerateFloor()
+end
+
+GenerateLeaderboards()
+
+print(Output)
